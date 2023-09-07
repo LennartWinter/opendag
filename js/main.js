25b30af10ff578bc5176f5960ce2eb7a45fecc2d
D296433 <D296433@edu.curio.nl>
2023-09-07 10:23:41 +0200

    Basis structuur paginas

--- js/main.js
+++ js/main.js
@@ -14,10 +14,15 @@ function replaceTextWithFileContent(file, id) {
       .catch(error => {
         console.error("An error occurred:", error);
       });
-  }
+}
+
+function replaceText(text, id){
+    var element = document.getElementById(id);
+    element.textContent = text;
+}
 
 function reload(url){
-    if(url == ""){
+    if(url == "" || url == null){
         location.reload();
     }
     else{
@@ -27,16 +32,21 @@ function reload(url){
 
 function study() {
     replaceTextWithFileContent("docs/study.txt", "text");
+    replaceText("Opleiding's traject", "title");
 }
 function home() {
     replaceTextWithFileContent("docs/home.txt", "text");
+    replaceText("Home", "title");
 }
 function about() {
     replaceTextWithFileContent("docs/about.txt", "text");
+    replaceText("Over de opleiding", "title");
 }
 function teachers() {
     replaceTextWithFileContent("docs/teachers.txt", "text");
+    replaceText("De docenten", "title");
 }
 function opinion() {
     replaceTextWithFileContent("docs/opinion.txt", "text");
+    replaceText("Wat vind ik van de opleiding?", "title");
 }
